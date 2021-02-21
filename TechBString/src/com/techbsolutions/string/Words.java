1cda538bde362585faa61c28a2ef523cc6fb7f5f
NiJA BINOY <nija@NiJAs-MacBook-Air.local>
2021-02-20 22:05:58 -0800

    Revert "same old project"
    This reverts commit ff6626abd3ee538326fbf8ea5b39da3376da5e44.

--- TechBString/src/com/techbsolutions/string/Words.java
+++ TechBString/src/com/techbsolutions/string/Words.java
@@ -1,26 +1,14 @@
 package com.techbsolutions.string;
 
-/**
- * Create Words class in JAVA and then do the following:
-- Ask User to enter a sentence or create a String variable to assign
- a sentence to it
-- Count all Words in a String and display the count
-- Display the count of word in a sentence
- * @author nija
- *
- */
 public class Words {
 
 	private static int countWords(String sentence) {
-		if(sentence==null || sentence.trim().length()==0) {
-			return 0;
-		}
-		String[] array=sentence.trim().split("\\s+");
+		String[] array=sentence.split(" ");		
 		return array.length;
 		
 	}
 	public static void main(String[] args) {
-		System.out.println(countWords("Can      you count how many words here?"));
+		System.out.println(countWords("Can you count how many words here?"));
 	}
 
 }
